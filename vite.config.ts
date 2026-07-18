9bb8cb6c3260cb60544ba4afc89c11b7db4780b0
Johannes Schießl <150372753+johannesschiessl@users.noreply.github.com>
2026-07-18 17:02:07 +0200

    Ignore .repos in test, fmt and lint

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from "vite-plus";
 
 export default defineConfig({
-  fmt: {},
+  test: {
+    exclude: [".repos/**"],
+  },
+  fmt: {
+    ignorePatterns: [".repos/**"],
+  },
   lint: {
     jsPlugins: [{ name: "vite-plus", specifier: "vite-plus/oxlint-plugin" }],
     rules: { "vite-plus/prefer-vite-plus-imports": "error" },
     options: { typeAware: true, typeCheck: true },
+    ignorePatterns: [".repos/**"],
   },
   run: {
     cache: true,
